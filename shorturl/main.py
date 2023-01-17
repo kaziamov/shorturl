07e5f55c75115045b6bdf19ff1c828976dbe4156
Ilia Kaziamov <kaziamov@outlook.com>
2023-01-17 22:03:29 +0300

    Add generate functions and tests

--- shorturl/main.py
+++ shorturl/main.py
@@ -1,2 +1,25 @@
+from string import ascii_letters, digits
+from random import choices
+
+DEFAULT_LENGTH = 5
+DEFAULT_SYMBOLS = ascii_letters + digits + '_!-'
+UNIQUE_SYM_COUNT = len(DEFAULT_SYMBOLS) ** DEFAULT_LENGTH
+
+
 def main():
-    pass
+    print(generate_path())
+
+
+def generate_link(custom=False):
+    if custom:
+        return custom
+    return generate_path()
+
+
+def generate_path():
+    return choices(DEFAULT_SYMBOLS, k=DEFAULT_LENGTH)
+
+
+if __name__ == "__main__":
+    main()
+    # print(f"Unique {UNIQUE_SYM_COUNT}")
--- tests/test_shorturl.py
+++ tests/test_shorturl.py
@@ -1,2 +1,7 @@
-def test_test():
-    assert 1 == 1
+from shorturl.main import DEFAULT_LENGTH, DEFAULT_SYMBOLS, generate_link, generate_path
+
+def test_generate_path_lenght():
+    assert len(generate_path()) == DEFAULT_LENGTH
+
+def test_generate_custom_link():
+    assert generate_link(custom='hello') == 'hello'
